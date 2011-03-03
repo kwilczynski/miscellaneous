e784082f37ff0b164f85576ed93f116743d0fa61
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-03-03 10:01:00 +0000

    First version.  This code allows for restoring all minimised windows and can be used as a script etc ...

---
+++ restore_windows.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+#
+# This script will attempt to restore all minimised windows ...
+#
+# You can make it a desktop icon or Gnome Panel launcher or
+# bind it to a particular keyboard short-cut etc.  Whatever works for you.
+#
+# General usefulness of this script is rather low ...
+#
+
+XPROP_BINARY   = '/usr/bin/xprop'
+WM_CTRL_BINARY = '/usr/bin/wmctrl'
+
+WINDOW_STATE_ATOM     = '_NET_WM_STATE'
+WINDOW_STATE_PROPERTY = '_NET_WM_STATE_HIDDEN'
+
+# We need to have these two to be able to do the job ...
+exit(1) unless File.exists?(WM_CTRL_BINARY) and File.exists?(XPROP_BINARY)
+
+result = %x{ #{WM_CTRL_BINARY} -l }
+result.each do |i|
+  if match = i.match(/(0x.+?)\s+0/)
+    window = match[1].strip
+    result = %x{ #{XPROP_BINARY} -id #{window} #{WINDOW_STATE_ATOM} }
+    if result.match(/#{WINDOW_STATE_PROPERTY}/)
+      %x{ #{WM_CTRL_BINARY} -i -a #{window} &> /dev/null }
+    end
+  end
+end
+
+exit(0)
