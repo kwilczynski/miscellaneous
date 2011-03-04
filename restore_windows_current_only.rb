326250432f1e6ee85edc6d61a6634f4067383bb1
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-03-04 23:38:27 +0000

    First version.  Same behaviour as restore_windows.rb but works only for desktop which is currently active.

---
+++ restore_windows_current_only.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+#
+# This script will attempt to restore all minimised windows on the desktop
+# which is currently active if you have more than one virtual desktop ...
+#
+# You can make it a desktop icon or Gnome Panel launcher or bind it to a
+# particular keyboard short-cut etc.  Whatever works for you.
+#
+# General usefulness of this script is rather low ...
+#
+
+XPROP_BINARY   = '/usr/bin/xprop'
+WM_CTRL_BINARY = '/usr/bin/wmctrl'
+
+WINDOW_STATE_ATOM     = '_NET_WM_STATE'
+WINDOW_STATE_PROPERTY = '_NET_WM_STATE_HIDDEN'
+
+# We need to have these two to be able to do the job ...
+exit(1) unless File.exists?(WM_CTRL_BINARY) and File.exists?(XPROP_BINARY)
+
+current_desktop = 0 # We assume that first desktop is the current one ...
+
+# First step is to detect current desktop ...
+result = %x{ #{WM_CTRL_BINARY} -d }
+result.each do |i|
+  if match = i.match(/^(\d+)\s+\*\s/)
+    current_desktop = match[1].strip
+  end
+end
+
+# Second step is to restore all minimised windows on current desktop only ...
+result = %x{ #{WM_CTRL_BINARY} -l }
+result.each do |i|
+  if match = i.match(/(0x.+?)\s+#{current_desktop}/)
+    window = match[1].strip
+    result = %x{ #{XPROP_BINARY} -id #{window} #{WINDOW_STATE_ATOM} }
+    if result.match(/#{WINDOW_STATE_PROPERTY}/)
+      %x{ #{WM_CTRL_BINARY} -i -a #{window} &> /dev/null }
+    end
+  end
+end
+
+exit(0)
