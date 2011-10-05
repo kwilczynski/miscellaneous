a57ff46bef4e53f3c23ddeac99c2e84e5f82a9a3
Krzysztof Wilczynski <krzysztof.wilczynski@youdevise.com>
2011-10-05 15:52:33 +0100

    Add better error handling.  Clean-up code and make it more Bash-alike.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@youdevise.com>

--- select_secondary_member.sh
+++ select_secondary_member.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#
 # select_secondary_member.sh
 #
 # Select first available Secondary member in the Replica Sets and show its
@@ -10,21 +11,26 @@ function select_secondary_member {
   # We will use indirect-reference hack to return variable from this function.
   local __return=$1
 
-  secondary=''
-
   # Retrieve list of Replica Sets members from local MongoDB instance.
   members=( $(mongo --quiet --eval \
-              'rs.isMaster().hosts.forEach(function(x) { print(x) })') )
+              'rs.isMaster().hosts.forEach(function(x) { print(x) })' 2> /dev/null) )
 
-  if [[ ! $? == 0 ]] ; then
+  if [ ! $? == 0 ] ; then
     echo "ERROR: Unable to retrieve list of Replica Sets members from local MongoDB instance." >&2
     return 1
   else
     # Process list of Replica Sets members and look for any Secondary ...
     if [[ ${#members[@]} > 1 ]] ; then
       for member in "${members[@]}" ; do
+        master_status="$(mongo --quiet --host $member --eval 'rs.isMaster().ismaster' 2> /dev/null)"
+
+        if [ ! $? == 0 ] ; then
+            echo "WARNING: Unable to query $member for its configuration details."
+            continue
+        fi
+
         # Check if Seconday?  If so then break ...
-        case "$(mongo --quiet --host $member --eval 'rs.isMaster().ismaster')" in
+        case "$master_status" in
           'true')
             # Skip particular member if it is a Primary.
             continue
@@ -43,7 +49,7 @@ function select_secondary_member {
     fi
 
     # Nothing found?  Then abort ...
-    if [[ -z "$secondary" ]] ; then
+    if [ -z "$secondary" ] ; then
       echo "ERROR: No suitable Secondary found in the Replica Sets." >&2
       return 1
     else
@@ -53,10 +59,7 @@ function select_secondary_member {
   fi
 }
 
-# Details of the Secondary member will be stored here ...
-secondary=''
-
-if [[ -z "$(which mongo)" ]] ; then
+if [ -z "$(which mongo)" ] ; then
   echo "ERROR: Unable to locate Mongo Shell binary." >&2
   exit 1
 fi
@@ -64,7 +67,7 @@ fi
 # Return value via indirect-reference hack ...
 select_secondary_member seconday
 
-if [[ -n "$secondary" ]] ; then
+if [ -n "$secondary" ] ; then
   echo "Available Replica Sets secondary: $secondary"
 else
   exit $?
