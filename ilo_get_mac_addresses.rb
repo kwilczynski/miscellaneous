ccaeeeba0c5a4696f6328c43107641a1ec50a9fe
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-12-11 14:11:39 +0000

    Fix white spaces.
    Concert tabs to spaces. No idea how that has happened ...
    
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- ilo_get_mac_addresses.rb
+++ ilo_get_mac_addresses.rb
@@ -32,9 +32,9 @@ DEFAULT_ILO_NIC_PATTERN = '[Pp]ort\d+NIC_.+'
 
 # Static map of hosts to iLO passwords ...
 hosts = { 'host-001' => 'XXXXXXX1',
-	  'host-002' => 'XXXXXXX2',
-	  'host-003' => 'XXXXXXX3',
-	  'host-004' => 'XXXXXXX4' }
+          'host-002' => 'XXXXXXX2',
+          'host-003' => 'XXXXXXX3',
+          'host-004' => 'XXXXXXX4' }
 
 if $0 == __FILE__
   mutex = Mutex.new
@@ -49,63 +49,63 @@ if $0 == __FILE__
   hosts.each do |host,password|
     threads << Thread.new(host) do |h|
       begin
-	timeout(DEFAULT_EXECUTION_TIME_OUT) do
-	  Net::SSH.start(
-	    host,
-	    DEFAULT_ILO_USER_NAME,
-	    :password => password,
-	    :paranoid => false
-	  ) do |ssh|
-	    ssh.exec("show #{DEFAULT_ILO_NICS_PATH}") do |channel, stream, data|
-	      #
-	      # Check whether there was any error.  Technically there should
-	      # not be any as we are working with remote appliance which is
-	      # noting alike Bash shell etc ...
-	      #
-	      if stream == :stderr
-		mutex.synchronize do
-		  configuration[host][:error] = "An error occurred: #{data}"
-		end
+        timeout(DEFAULT_EXECUTION_TIME_OUT) do
+          Net::SSH.start(
+            host,
+            DEFAULT_ILO_USER_NAME,
+            :password => password,
+            :paranoid => false
+          ) do |ssh|
+            ssh.exec("show #{DEFAULT_ILO_NICS_PATH}") do |channel, stream, data|
+              #
+              # Check whether there was any error.  Technically there should
+              # not be any as we are working with remote appliance which is
+              # noting alike Bash shell etc ...
+              #
+              if stream == :stderr
+                mutex.synchronize do
+                  configuration[host][:error] = "An error occurred: #{data}"
+                end
 
-		break
-	      elsif stream == :stdout
-		# We should have network cards details for processing already ...
-		data.split("\n").each do |line|
-		  # Remove bloat ...
-		  line.strip!
+                break
+              elsif stream == :stdout
+                # We should have network cards details for processing already ...
+                data.split("\n").each do |line|
+                  # Remove bloat ...
+                  line.strip!
 
-		  # Skip new and empty lines ...
-		  next if line.match(/^(\r\n|\n|\s*)$|^$/)
+                  # Skip new and empty lines ...
+                  next if line.match(/^(\r\n|\n|\s*)$|^$/)
 
-		  if line.match(DEFAULT_ILO_MAC_PATTERN)
-		    address = line.split('=')[1]
+                  if line.match(DEFAULT_ILO_MAC_PATTERN)
+                    address = line.split('=')[1]
 
-		    mutex.synchronize do
-		      configuration[host][:ilo] = address
-		    end
-		  elsif line.match(DEFAULT_ILO_NIC_PATTERN)
-		    address = line.split('=')[1]
+                    mutex.synchronize do
+                      configuration[host][:ilo] = address
+                    end
+                  elsif line.match(DEFAULT_ILO_NIC_PATTERN)
+                    address = line.split('=')[1]
 
-		    mutex.synchronize do
-		      (configuration[host][:nics] ||= []) << address
-		    end
-		  else
-		    # Skip irrelevant entries ...
-		    next
-		  end
-		end
-	      end
-	    end
-	  end
-	end
+                    mutex.synchronize do
+                      (configuration[host][:nics] ||= []) << address
+                    end
+                  else
+                    # Skip irrelevant entries ...
+                    next
+                  end
+                end
+              end
+            end
+          end
+        end
       rescue Timeout::Error
-      	configuration[host][:error] = 'Execution timed out'
+        configuration[host][:error] = 'Execution timed out'
       rescue Errno::ETIMEDOUT
-      	configuration[host][:error] = 'Connection timed out'
+        configuration[host][:error] = 'Connection timed out'
       rescue Errno::EHOSTUNREACH
-	configuration[host][:error] = 'Host unreachable'
+        configuration[host][:error] = 'Host unreachable'
       rescue => e
-	configuration[host][:error] = "Unknown error: #{e.to_s}"
+        configuration[host][:error] = "Unknown error: #{e.to_s}"
       end
     end
   end
@@ -120,9 +120,9 @@ if $0 == __FILE__
 
     details.each do |k,v|
       if v.is_a?(Array)
-	v.each_with_index do |entry,index|
-	  puts "\tNetwork Card #{index + 1} MAC Address: #{entry}"
-	end
+        v.each_with_index do |entry,index|
+          puts "\tNetwork Card #{index + 1} MAC Address: #{entry}"
+        end
       else
         case k
         when :ilo
